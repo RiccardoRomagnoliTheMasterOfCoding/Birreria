f513a094da47b557f075b777ea7cd1ad6d3e5501
riccardo.romagnoli <riccardo.romagnoli.stud@ispascalcomandini.it>
2024-11-04 09:28:05 +0100

    Aggiunta descrizione

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ class Beer {
     id: number;
     name: string;
     price: number;
+    description: string;
 }
 
 class Order {
@@ -11,20 +12,20 @@ class Order {
 }
 
 let menu: Beer[] = [
-    { id: 0, name: "Peroni", price: 4.00 },
-    { id: 1, name: "Ceres", price: 4.20 },
-    { id: 2, name: "Heineken", price: 3.50 },
-    { id: 3, name: "Ichnusa", price: 6.90 },
-    { id: 4, name: "Corona", price: 4.00 }
+    { id: 0, name: "Peroni", price: 4.00, description: "S" },
+    { id: 1, name: "Ceres", price: 4.20, description: "C'è" },
+    { id: 2, name: "Heineken", price: 3.50, description: "verde" },
+    { id: 3, name: "Ichnusa", price: 6.90, description: "Sas" },
+    { id: 4, name: "Corona", price: 4.00, description: "I call her chandelier!" }
 ]
 
 let idOrder: number = 0;
 let totalPrice: number = 0;
 let orders: Order[] = [];
 
-function createNewBeer(name: string, price: number) {
+function createNewBeer(name: string, price: number, description: string) {
     if (isFinite(price) && price > 0) {
-        menu.push({ id: menu[menu.length - 1].id + 1, name: name, price: price });
+        menu.push({ id: menu[menu.length - 1].id + 1, name: name, price: price, description: description });
         populateBeerList(menu);
         menu.forEach(beer => {
             updateUI(beer.id, document.getElementById(`${beer.id}_requested`));
@@ -42,7 +43,7 @@ function populateBeerList(menu: Beer[]) {
                         <div class="fw-bold">${beer.name} (${beer.price}€)
                             <span class="badge rounded-pill text-bg-info mx-3" id="${beer.id}_requested">0</span>
                         </div>
-                        <div id="${beer.id}_description">è buona</div>
+                        <div id="${beer.id}_description">${beer.description}</div>
                     </div>
                     <button class="btn btn-success me-1" onclick="addBeer(${beer.id})">
                         <i class="bi bi-plus-circle h3"></i>
